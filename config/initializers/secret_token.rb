47416bfd92dd65cff081e4e1f6edacc26a5dc96f
Aaron Trostle <aaron.trostle@gmail.com>
2013-05-29 12:44:24 -0700

    Initial commit with updated readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InterviewScheduler::Application.config.secret_token = '519320e4466a737dd6ac3c74e55981e61d07d4bc1986f957aca6f2368e5f069d30410ac9952d627ba5901ac3c1e28dc0a39e373e8d67c597dad3c4d26628a264'
